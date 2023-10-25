0fe66de9a356eb65152b8ded7a4dedd0123a48de
Molly <erdy521@gmail.com>
2023-10-25 15:50:57 +0800

    feat:ListItem

--- src/Sidebar/ListItem.js
+++ src/Sidebar/ListItem.js
@@ -1,10 +1,12 @@
 import "./ListItem.css";
 
-const ListItem = ({ imgSrc, content }) => {
-  <div className="listItem">
-    <img className="listItem_img" src={imgSrc} alt="" />
-    <div className="listItem_content">{content}</div>
-  </div>;
+const ListItem = ({ imgSrc, content, onClick }) => {
+  return (
+    <div className="listItem" onClick={onClick}>
+      <img className="listItem_img" src={imgSrc} alt="" />
+      <div className="listItem_content">{content}</div>
+    </div>
+  );
 };
 
 export default ListItem;
--- src/Sidebar/ShortCut.js
+++ src/Sidebar/ShortCut.js
@@ -1,41 +1,58 @@
 import "./ShortCut.css";
+import ListItem from "./ListItem";
+import React, { useState } from "react";
 
 const ShortCut = () => {
+  const [showMore, setShowMore] = useState(false);
+
+  const handleShowMore = () => {
+    setShowMore(!showMore);
+  };
   return (
     <div>
       <div className="shortCut_title">你的捷徑</div>
-      <div className="taiwan">
-        <img
-          className="taiwan_img"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeBnPWBBUG_FQ3uzw5La5W-LJD6l7yr7k6VRVM5u3BhelgoDTf7Ki8QIciRnqOEY7Gbt4&usqp=CAU"
-          width="25"
-        ></img>
-        <div className="taiwan_content">我是台灣人</div>
-      </div>
-      <div className="ski">
-        <img
-          className="ski_img"
-          src="https://thumb.silhouette-ac.com/t/04/04a29e068842a1cac137bf1bcf7e6da7_t.jpeg"
-          width="25"
-        ></img>
-        <div className="ski_content">滑雪討論區</div>
-      </div>
-      <div className="badminton">
-        <img
-          className="badminton_img"
-          src="https://cdn.icon-icons.com/icons2/433/PNG/512/badminton_42657.png"
-          width="25"
-        ></img>
-        <div className="badminton_content">羽球交流版</div>
-      </div>
-      <div className="showMore">
-        <img
-          className="showMore_img"
-          src="https://img.88icon.com/download/jpg/20200716/c96ab94f31bc0e1ebb1eaa99e89a7f7d_512_512.jpg!bg"
-          width="25"
-        ></img>
-        <div className="showMore_content">顯示更多</div>
-      </div>
+      <ListItem
+        imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeBnPWBBUG_FQ3uzw5La5W-LJD6l7yr7k6VRVM5u3BhelgoDTf7Ki8QIciRnqOEY7Gbt4&usqp=CAU"
+        content="我是台灣人"
+      />
+      <ListItem
+        imgSrc="https://thumb.silhouette-ac.com/t/04/04a29e068842a1cac137bf1bcf7e6da7_t.jpeg"
+        content="滑雪討論區"
+      />
+      <ListItem
+        imgSrc="https://cdn.icon-icons.com/icons2/433/PNG/512/badminton_42657.png"
+        content="羽球交流版"
+      />
+      {showMore && (
+        <div>
+          <ListItem
+            imgSrc="https://media.istockphoto.com/id/1168022130/zh/%E5%90%91%E9%87%8F/%E9%A3%9B%E6%A9%9F%E5%9C%96%E7%A4%BA%E5%90%91%E9%87%8F.jpg?s=170667a&w=0&k=20&c=ZzTFg4rD4MufYwBSA_yBDOiASjvSGeCzSaNffTj_bxA="
+            content="機票研究室"
+          />
+          <ListItem
+            imgSrc="https://cdn-icons-png.flaticon.com/512/4488/4488318.png"
+            content="二手樂器買賣交流"
+          />
+          <ListItem
+            imgSrc="https://cdn3.iconfinder.com/data/icons/illustricon-tech/512/browser.development-512.png"
+            content="Frontend 台灣"
+          />
+          <ListItem
+            imgSrc="https://play-lh.googleusercontent.com/COGhjzBBD0F3ky0iplKrN1NsKAGBrEapdAJX391uHHB2ZiT5bHi4n_WhNLuvCgkYq6g"
+            content="跑跑交流區"
+          />
+        </div>
+      )}
+
+      <ListItem
+        imgSrc={
+          showMore
+            ? "https://thumb.silhouette-ac.com/t/bb/bbee358230192723d97e3597f9224f07_t.jpeg"
+            : "https://img.88icon.com/download/jpg/20200716/c96ab94f31bc0e1ebb1eaa99e89a7f7d_512_512.jpg!bg"
+        }
+        content={showMore ? "顯示更少" : "顯示更多"}
+        onClick={handleShowMore}
+      />
     </div>
   );
 };
